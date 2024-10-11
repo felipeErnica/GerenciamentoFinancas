20f5a72aec0514b4a74e701a802c09584637c260
felipeErnica <felipe.ernica@gmail.com>
2024-10-10 21:02:07 -0300

    Form Banco static method

--- src/main/java/com/santacarolina/areas/bancario/banco/frmManageBanco/ManageBancoForm.java
+++ src/main/java/com/santacarolina/areas/bancario/banco/frmManageBanco/ManageBancoForm.java
@@ -1,9 +1,25 @@
 package com.santacarolina.areas.bancario.banco.frmManageBanco;
 
+import com.santacarolina.GerenciamentoFinancas;
+import com.santacarolina.exceptions.FetchFailException;
+import com.santacarolina.util.CustomErrorThrower;
+
 public class ManageBancoForm {
 
-    public static void open() {
+    public static void main(String[] args) {
+        GerenciamentoFinancas.setStyle();
+        open();
+    }
 
+    public static void open() {
+        try {
+            FormView view = new FormView();
+            TableModel model = new TableModel();
+            FormController controller = new FormController(view, model);
+            controller.showView();
+        } catch (FetchFailException e) {
+            CustomErrorThrower.throwError(e);
+        }
     }
 
 }
