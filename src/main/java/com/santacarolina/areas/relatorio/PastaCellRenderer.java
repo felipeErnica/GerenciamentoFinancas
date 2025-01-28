af8b780bfcfb76eedebc158be946b3be4dea5bd8
felipeErnica <felipe.ernica@gmail.com>
2025-01-28 08:41:58 -0300

    Mudanças na Lista de Pastas

--- src/main/java/com/santacarolina/areas/relatorio/PastaCellRenderer.java
+++ src/main/java/com/santacarolina/areas/relatorio/PastaCellRenderer.java
@@ -2,6 +2,7 @@ package com.santacarolina.areas.relatorio;
 
 import java.awt.Component;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JCheckBox;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -10,10 +11,14 @@ import com.santacarolina.model.PastaContabil;
 
 public class PastaCellRenderer implements ListCellRenderer<PastaContabil> {
 
+    private DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+    
+
     @Override
-    public Component getListCellRendererComponent(JList<? extends PastaContabil> listaPasta, PastaContabil pasta, int arg2,
-            boolean arg3, boolean arg4) {
-        JCheckBox checkBox = new JCheckBox(pasta.getNome());
+    public Component getListCellRendererComponent(JList<? extends PastaContabil> listaPasta, PastaContabil pasta, int index,
+            boolean isSelected, boolean cellHasFocus) {
+        JCheckBox checkBox = (JCheckBox) defaultRenderer.getListCellRendererComponent(listaPasta, pasta, index, isSelected, cellHasFocus);
         return checkBox;
     }
 
